2c15aaac71e79f4d8ecc99072187c2284da0bd8c
yosra mekaoui <yosra.mekaoui@esprit.tn>
2022-10-31 11:16:14 +0100

    Create ProductController.java

---
+++ src/main/java/com/ensat/controllers/ProductController.java
@@ -0,0 +1,89 @@
+package com.ensat.controllers;
+
+import com.ensat.entities.Product;
+import com.ensat.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+/**
+ * Product controller.
+ */
+@RestController("/products")
+public class ProductController {
+     @Autowired
+     private ProductService productService;
+
+   
+
+    /**
+     * List all products.
+     *
+     * @param model
+     * @return
+     */
+   @GetMapping("/")
+    public String list(Model model) {
+        model.addAttribute("products", productService.listAllProducts());
+        System.out.println("Returning products:");
+        return "products";
+    }
+
+    /**
+     * View a specific product by its id.
+     *
+     * @param id
+     * @param model
+     * @return
+     */
+    @GetMapping("/{id}")
+    public String showProduct(@PathVariable Integer id, Model model) {
+        model.addAttribute("product", productService.getProductById(id));
+        return "productshow";
+    }
+
+    // Afficher le formulaire de modification du Product
+    @PutMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        model.addAttribute("product", productService.getProductById(id));
+        return "productform";
+    }
+
+    /**
+     * New product.
+     *
+     * @param model
+     * @return
+     */
+    @RequestMapping("product/new")
+    public String newProduct(Model model) {
+        model.addAttribute("product", new Product());
+        return "productform";
+    }
+
+    /**
+     * Save product to database.
+     *
+     * @param product
+     * @return
+     */
+    @RequestMapping(value = "product", method = RequestMethod.POST)
+    public String saveProduct(Product product) {
+        productService.saveProduct(product);
+        return "redirect:/product/" + product.getId();
+    }
+
+    /**
+     * Delete product by its id.
+     *
+     * @param id
+     * @return
+     */
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable Integer id) {
+        productService.deleteProduct(id);
+        return "redirect:/products";
+    }
+
+}
